5be7ea2f7d87e007ba28bf18c736d0dba2e06bd0
Daniel Alejandro Alfonso <dalfonso@epik.com.co>
2021-09-08 19:52:35 -0500

    New Scope: Read Google Sheets File

---
+++ APIServicios/APIServicios/Controllers/RegistroController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIServicios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistroController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public IEnumerable<Object> Get()
+        {
+            var rng = new Random();
+            return new List<Object>() ;
+        }
+    }
+}
---
+++ APIServicios/APIServicios/Negocio/ISheetsReaderBusiness.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIServicios.Negocio
+{
+    public interface ISheetsReader
+    {
+        public byte[] readSheet();
+    }
+}
